973b95dddadbe079358639b6bb9a4c52a53ea469
B2guilherme <guilhermecrm18@gmail.com>
2026-05-15 20:09:24 -0300

    doador de sangue

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int idade;
+
+Console.Write("Qual a sua idade: ");
+
+idade = Convert.ToInt32(Console.ReadLine());
+
+if(idade >= 18 && idade <= 69)
+{
+    Console.WriteLine("Voce pode ser doador de sangue ");
+}
+else
+{
+    Console.WriteLine("Voce nao pode ser doador de sangue ");
+}
